dadd158aebfe9f7a3e51507de883a21fdf39646c
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-04-20 02:24:33 +0300

    home page initial ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_user/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text('Random User'),
+      home: const HomePage(),
     );
   }
 }

1f7ec18f05d4eca0487fc75bb3b77a9b17477322
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-04-21 01:52:00 +0300

    dart ci improved

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:random_user/app.dart';
 import 'package:random_user/home_page.dart';
-import 'package:random_user/main.dart';
 
 void main() {
   testWidgets('testing HomePage shows up', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RandomUser());
 
     final homePageFinder = find.byType(HomePage);
 

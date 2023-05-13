c745bedd9e9b3e850ac3c099795b4f5d7a2480d9
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-05-13 20:31:36 +0300

    init env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:random_user/app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.DotEnv().load(fileName: "secrets.env");
+
   runApp(
     const ProviderScope(
       child: RandomUser(),

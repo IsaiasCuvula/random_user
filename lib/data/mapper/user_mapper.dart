56e5e439d00e1dd0b8dcfbec769ed27f0c231277
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-05-18 01:49:12 +0300

    refactored

--- lib/data/mapper/user_mapper.dart
+++ lib/data/mapper/user_mapper.dart
@@ -1,34 +1,19 @@
 import 'package:random_user/data/data.dart';
 import 'package:random_user/domain/domain.dart';
+import 'package:random_user/presentation/utils/utils.dart';
 
 class UserMapper {
   static RandomUserModel fromJson(Map<String, dynamic> map) {
-    final user = map['results'][0];
-    return RandomUserModel(
-      gender: user['gender'],
-      name: Username.fromJson(user['name']),
-      location: Location.fromJson(user['location']),
-      nat: user['nat'],
-      picture: Picture.fromJson(user['picture']),
-      phone: user['phone'],
-      email: user['email'],
-    );
+    final user = map['results'].first;
+    return _mapToRandomUserModel(user);
   }
 
-  static List<RandomUserModel> fromJsonList(Map<String, dynamic> map) {
+  static ListUsersModel fromJsonList(Map<String, dynamic> map) {
     final usersResult = map['results'];
-    List<RandomUserModel> usersList = [];
+    ListUsersModel usersList = [];
 
     for (var user in usersResult) {
-      final randomUser = RandomUserModel(
-        gender: user['gender'],
-        name: Username.fromJson(user['name']),
-        location: Location.fromJson(user['location']),
-        nat: user['nat'],
-        picture: Picture.fromJson(user['picture']),
-        phone: user['phone'],
-        email: user['email'],
-      );
+      final randomUser = _mapToRandomUserModel(user);
       usersList.add(randomUser);
     }
     return usersList;
@@ -43,6 +28,18 @@ class UserMapper {
       picture: model.picture,
       phone: model.phone.toString(),
       email: model.email.toString(),
+    );
+  }
+
+  static RandomUserModel _mapToRandomUserModel(dynamic user) {
+    return RandomUserModel(
+      gender: user['gender'],
+      name: Username.fromJson(user['name']),
+      location: Location.fromJson(user['location']),
+      nat: user['nat'],
+      picture: Picture.fromJson(user['picture']),
+      phone: user['phone'],
+      email: user['email'],
     );
   }
 }

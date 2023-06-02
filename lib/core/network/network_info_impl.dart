74a42d7a662c92607431eb081ea833e973c88c13
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-06-03 01:02:35 +0300

    clean & improved

--- lib/core/network/network_info_impl.dart
+++ lib/core/network/network_info_impl.dart
@@ -2,16 +2,14 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:random_user/core/core.dart';
 
 class NetworkInfoImpl implements NetworkInfo {
-  final Connectivity connectivity;
-
-  NetworkInfoImpl(this.connectivity);
+  NetworkInfoImpl();
   @override
   Future<bool> get isConnected => _isConnected();
 
   Future<bool> _isConnected() async {
-    final connectivityCheck = await connectivity.checkConnectivity();
-    return connectivityCheck == ConnectivityResult.mobile ||
-        connectivityCheck == ConnectivityResult.wifi ||
-        connectivityCheck == ConnectivityResult.ethernet;
+    final connectivity = await Connectivity().checkConnectivity();
+    return connectivity == ConnectivityResult.mobile ||
+        connectivity == ConnectivityResult.wifi ||
+        connectivity == ConnectivityResult.ethernet;
   }
 }
--- lib/core/network/network_info_provider.dart
+++ lib/core/network/network_info_provider.dart
@@ -1,8 +1,6 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:random_user/core/core.dart';
 
 final networkInfoProvider = Provider.autoDispose<NetworkInfo>((ref) {
-  final connectivity = Connectivity().checkConnectivity();
-  return NetworkInfoImpl(connectivity as Connectivity);
+  return NetworkInfoImpl();
 });

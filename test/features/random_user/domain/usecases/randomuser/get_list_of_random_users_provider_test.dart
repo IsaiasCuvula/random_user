160f8279712474563c174abc09b6cf8e8927a229
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-06-10 00:42:32 +0300

    unit test

---
+++ test/features/random_user/domain/usecases/randomuser/get_list_of_random_users_provider_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:random_user/features/random_user/random_user.dart';
+import 'package:random_user/core/core.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+void main() {
+  late ProviderContainer providerContainer;
+  late Database database;
+
+  setUpAll(() async {
+    providerContainer = ProviderContainer();
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+    database = await databaseFactoryFfi.openDatabase(
+      inMemoryDatabasePath,
+      options: OpenDatabaseOptions(
+        version: DbConstants.databaseVersion,
+      ),
+    );
+    await database.execute(DbConstants.createTableQuery);
+  });
+
+  tearDown(() {
+    providerContainer.dispose();
+  });
+
+  test(
+      'getListRandomUsersUseCaseProvider should returns an instance of GetListOfRandomUsers...',
+      () {
+    //arrange
+
+    //act
+    final result = providerContainer.read(getListRandomUsersUseCaseProvider);
+
+    //assert
+    expect(result, isA<GetListOfRandomUsers>());
+  });
+}
